a1f67e7f064f220e6a1ff7fd05658d02b5bca4da
Laowpetch <ton290745@gmail.com>
2021-08-29 14:56:50 +0700

    torKhamHanSaa complete

--- Python2/torKhamHanSaa.py
+++ Python2/torKhamHanSaa.py
@@ -1,8 +1,9 @@
+import sys
 class TorKham :
     def __init__(self,name):
         self.name = name
         self.listKham = []
-        
+        self.index = 0      
     def catagorize(self,char_,kham):
         self.char_ = char_
         self.kham = kham
@@ -14,6 +15,7 @@ class TorKham :
             self.end_()
         else:
             print("'"+self.char_+" "+self.kham+"' is Invalid Input !!!")
+            sys.exit()
     def catagorize2(self,char_,):
         self.char_ = char_
         self.kham = ""
@@ -24,23 +26,28 @@ class TorKham :
         else:
             pass
     def continue_(self):
-        if len(self.listKham==0) or (str(self.kham[1]).upper() == str(self.listKham[-1][-2]).upper() and str(self.kham[2]).upper() == str(self.listKham[-1][-1]).upper()):
+        if self.index == 0 or (str(self.kham[0]).upper() == str(self.listKham[-1][-2]).upper() and str(self.kham[1]).upper() == str(self.listKham[-1][-1]).upper()):
             self.listKham.append(self.kham)
-            print("'"+self.kham+"' -> "+self.listKham)
+            print("'"+str(self.kham)+"' ->",self.listKham)
+            self.index += 1
         else:
             self.over()
+            self.index = 0
     def restart_(self):
         self.listKham.clear()
+        self.index = 0
         print('game restarted')
     def end_(self):
+        self.index = 0
         pass
     def over(self):
         print("'"+self.kham+"' -> game over")
+        self.index = 0
 
 torKham = TorKham("torKham")
 temp = []
 print('*** TorKham HanSaa ***')
-list_ = input('Enter Input : ').split(',')
+list_ = list(input('Enter Input : ').split(','))
 for i in range(0,len(list_)):
     temp = list_[i].split(' ')
     if len(temp) == 1:
