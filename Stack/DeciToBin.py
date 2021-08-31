7323dab4ca3bf55671991a5791d6e0a81ee557f7
Laowpetch <ton290745@gmail.com>
2021-08-31 19:13:52 +0700

    Complete

---
+++ Stack/DeciToBin.py
@@ -0,0 +1,17 @@
+class Stack :
+
+    ### Enter Your Code Here ###
+
+def dec2bin(decnum):
+
+    s = Stack()
+
+    ### Enter Your Code Here ###
+
+print(" ***Decimal to Binary use Stack***")
+
+token = input("Enter decimal number : ")
+
+print("Binary number : ",end='')
+
+print(dec2bin(int(token))
