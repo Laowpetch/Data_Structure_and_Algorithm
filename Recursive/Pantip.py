4994214fe94e09ff4743fd8407177f42ba9d4278
Laowpetch <ton290745@gmail.com>
2021-09-15 15:43:01 +0700

    pantip.py

--- Recursive/Pantip.py
+++ Recursive/Pantip.py
@@ -1,7 +1,11 @@
 def pantip(k, n, arr, path):
-    # Code Here
-    pass
-
+    if n == len(arr):
+        if k == sum(path):
+            print(*path)
+            return 1
+        return 0
+    return pantip(k,n+1,arr,path+[arr[n]]) + pantip(k,n+1,arr,path)
+        
 inp = input('Enter Input (Money, Product) : ').split('/')
 arr = [int(i) for i in inp[1].split()]
 pattern = pantip(int(inp[0]), 0, arr, [])
