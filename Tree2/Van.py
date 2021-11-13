4c072b007e9b13671f6b029f9ca6a348eb71cfa4
Laowpetch <ton290745@gmail.com>
2021-11-14 00:29:24 +0700

    Van

--- Tree2/Van.py
+++ Tree2/Van.py
@@ -1,3 +1,14 @@
 num,day = input('Enter Input : ').split('/')
 num = int(num)
+van = []
 day = [int(i) for i in day.split()]
+for i in range(0,num):van.append(0)
+for i in range(0,len(day)):
+    while not 0 in van:
+        for k in range(0,len(van)):
+            van[k] -= 1
+    for j in range(0,len(van)):
+        if van[j] == 0:
+            van[j] = day[i]
+            print('Customer',i+1,'Booking Van',j+1,'|',day[i],'day(s)')
+            break
