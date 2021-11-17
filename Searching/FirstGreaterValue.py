c2c8135572a72ccf581d5e504fa11dd6811cd2c9
Laowpetch <ton290745@gmail.com>
2021-11-17 23:41:09 +0700

    Searching

---
+++ Searching/FirstGreaterValue.py
@@ -0,0 +1,11 @@
+def first_greater_value(list_, value_):
+    list_ = sorted(list_)
+    for i in list_:
+        if i > value_:
+            return i
+    return 'No First Greater Value'
+
+inp = input("Enter Input : ").split('/')
+list_, value_ = list(map(int, inp[0].split())), list(map(int, inp[1].split()))
+for i in value_:
+    print(first_greater_value(list_, i))
